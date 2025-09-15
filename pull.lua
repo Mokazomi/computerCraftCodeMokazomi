9b278aca3ae390edb05c01b9c42d5eed09f01dec
mokazomi <abgoss52@gmail.com>
2025-09-14 19:44:47 -0500

    6

--- pull.lua
+++ pull.lua
@@ -2,6 +2,8 @@
 
 shell.run("wget https://raw.githubusercontent.com/Konijima/cc-git-clone/master/gitclone.lua")
 
-shell.run("rm computerCraftCodeMokazomi")
+if fs.exists("computerCraftCodeMokazomi") then
+    shell.run("rm computerCraftCodeMokazomi")
+end
 
 shell.run("gitclone Mokazomi computerCraftCodeMokazomi main ./")
