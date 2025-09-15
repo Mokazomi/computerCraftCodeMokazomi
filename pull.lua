abd77c799653e0b42ef470557341cea0c4253f03
mokazomi <abgoss52@gmail.com>
2025-09-14 19:36:44 -0500

    4

--- pull.lua
+++ pull.lua
@@ -2,4 +2,6 @@
 
 wget https://raw.githubusercontent.com/Konijima/cc-git-clone/master/gitclone.lua
 
+shell.run("rm computerCraftCodeMokazomi")
+
 shell.run("gitclone Mokazomi computerCraftCodeMokazomi main ./")
